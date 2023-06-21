edfe0b44ec4ce511dcd8ca98e82e3de88ad3f885
ENG AYUUB <moztaphem9@gmal.com>
2023-06-21 21:21:58 +0300

    Expensive-jewellery done3

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Good Luck 💪🏾
+function mostExpensive() {
+    const items = {
+      Diamond_Earrings: 980,
+      Gold_Watch: 250,
+      Pearl_Necklace: 4650,
+    };
+  
+    let maxPrice = 0;
+    let mostExpensiveItem = '';
+  
+    for (const item in items) {
+      if (items[item] > maxPrice) {
+        maxPrice = items[item];
+        mostExpensiveItem = item;
+      }
+    }
+  
+    console.log("THE MOST EXPENSIVE ONE IS", mostExpensiveItem);
+  }
+  
+  mostExpensive();
